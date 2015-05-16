ab9b3c93f170a0a7a8c506338c719e8076e03a00
Ohad Behore <ohadbehore@gmail.com>
2015-05-16 22:31:34 +0300

    First commit

---
+++ src/FileDataRedorder.cpp
@@ -0,0 +1,18 @@
+/*
+ * FileDataRedorder.cpp
+ *
+ *  Created on: 16 במאי 2015
+ *      Author: ohad
+ */
+
+#include "FileDataRedorder.h"
+
+FileDataRedorder::FileDataRedorder() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FileDataRedorder::~FileDataRedorder() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/FileDataRedorder.h
@@ -0,0 +1,17 @@
+/*
+ * FileDataRedorder.h
+ *
+ *  Created on: 16 במאי 2015
+ *      Author: ohad
+ */
+
+#ifndef FILEDATAREDORDER_H_
+#define FILEDATAREDORDER_H_
+
+class FileDataRedorder {
+public:
+	FileDataRedorder();
+	virtual ~FileDataRedorder();
+};
+
+#endif /* FILEDATAREDORDER_H_ */
